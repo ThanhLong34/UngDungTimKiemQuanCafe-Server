e01a96f80fc6ffd5670311ac7aa5e329d72481f0
ThanhLong34 <thanhlongedu0304@gmail.com>
2023-09-30 07:33:29 +0700

    Cập nhật index.js

--- src/config/mongoDB.js
+++ src/config/mongoDB.js
@@ -4,8 +4,10 @@ async function connect() {
 	try {
 		await mongoose.connect(process.env.MONGODB_CONNECT_STRING + process.env.MONGODB_DATABASE_NAME, {});
 		console.log("Kết nối MongoDB thành công");
+		return true
 	} catch (error) {
 		console.error('Kết nối MongoDB thất bại: ', error);
+		return false
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -12,64 +12,69 @@ const {
 	limitMiddleware,
 } = require("./app/middlewares");
 
-//? Kết nối MongoDB
-(async () => {
-	await mongoDBConnect.connect()
+//? Chạy server
+(async function startServer() {
+	//? Kết nối MongoDB
+	const connectResult = await mongoDBConnect.connect();
+	if (!connectResult) return;
+
+	//? Khởi tạo App
+	const app = express();
+	const PORT = process.env.PORT || 5000;
+	const fileUploader = multer({
+		storage: multer.diskStorage({
+			destination: (req, file, cb) => {
+				cb(null, "public/upload");
+			},
+			filename: function (req, file, cb) {
+				cb(
+					null,
+					file.fieldname +
+						"-" +
+						Date.now() +
+						path.extname(file.originalname)
+				);
+			},
+		}),
+	});
+
+	//? Middlewares
+	app.use(morgan("dev"));
+	app.use(
+		cors({
+			origin: "*",
+			methods: ["GET", "POST", "PUT", "DELETE"],
+		})
+	);
+	app.use(express.urlencoded({ extended: true })); // Middleware FormData HTML from request
+	app.use(express.json()); // Middleware JSON from request
+	app.use(sortMiddleware); // Sort middleware
+	app.use(searchMiddleware); // Search middleware
+	app.use(limitMiddleware); // Limit & Offset middleware
+
+	//? Static folder
+	app.use("/", express.static(path.join(__dirname, "../public")));
+
+	//? Cấu hình routes
+	configRouter(app);
+
+	//? Bắt lỗi không đúng URL
+	app.use((req, res, next) => {
+		res.json({
+			code: 999,
+			message: "URL không tồn tại",
+		});
+	});
+	app.use((err, req, res, next) => {
+		console.error(err);
+		res.json({
+			code: err.status,
+			message: err.message,
+		});
+	});
+
+	//? Khởi chạy máy chủ
+	app.listen(PORT, () => {
+		console.log(`Máy chủ đang chạy trên: http://localhost:${PORT}`);
+	});
 })();
-
-//? Khởi tạo App
-const app = express();
-const PORT = process.env.PORT || 5000;
-const fileUploader = multer({
-	storage: multer.diskStorage({
-		destination: (req, file, cb) => {
-			cb(null, "public/upload");
-		},
-		filename: function (req, file, cb) {
-			cb(
-				null,
-				file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-			);
-		},
-	}),
-});
-
-//? Middlewares
-app.use(morgan("dev"));
-app.use(
-	cors({
-		origin: "*",
-		methods: ["GET", "POST", "PUT", "DELETE"],
-	})
-);
-app.use(express.urlencoded({ extended: true })); // Middleware FormData HTML from request
-app.use(express.json()); // Middleware JSON from request
-app.use(sortMiddleware); // Sort middleware
-app.use(searchMiddleware); // Search middleware
-app.use(limitMiddleware); // Limit & Offset middleware
-
-//? Static folder
-app.use('/', express.static(path.join(__dirname, '../public')))
-
-//? Cấu hình routes
-configRouter(app);
-
-//? Bắt lỗi không đúng URL
-app.use((req, res, next) => {
-	res.json({
-		code: 999,
-		message: "URL không tồn tại",
-	});
-});
-app.use((err, req, res, next) => {
-	console.error(err);
-	res.json({
-		code: err.status,
-		message: err.message,
-	});
-});
-
-//? Khởi chạy máy chủ
-app.listen(PORT, () => {
-	console.log(`Máy chủ đang chạy trên: http://localhost:${PORT}`);
-});
